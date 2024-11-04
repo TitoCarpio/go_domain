89fc7f6741e773a9b29c816c221a4330392f5f4a
TitoCarpio <titocarpio9@gmail.com>
2024-11-03 18:29:02 -0600

    update enrollment.go

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -9,9 +9,9 @@ import (
 type Enrollment struct {
 	ID        string     `json:"id" gorm:"type:char(36);primary_key;unique_index;not null"`
 	UserID    string     `json:"user_id,omitempty" gorm:"type:char(36)"`
-	User      *User      `json:"user,omitempty"`
+	User      *User      `json:"user,omitempty"  gorm:"-"`
 	CourseID  string     `json:"course_id" gorm:"type:char(36);not null"`
-	Course    *Course    `json:"course,omitempty"`
+	Course    *Course    `json:"course,omitempty"  gorm:"-"`
 	Status    string     `json:"status" gorm:"type:char(2)"`
 	CreatedAt *time.Time `json:"-"`
 	UpdatedAt *time.Time `json:"-"`
